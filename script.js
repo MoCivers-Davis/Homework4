a02b769cdc3fe5aa32de0e5f9887c36be1344ca9
Mo Civ <mocivers@comcast.net>
2020-02-11 23:04:46 -0500

    updated JavaScript to include 1 question and answer buttons

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 const startButton = document.getElementById("start-btn")
 const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
 
 function startGame() {
     console.log("Started")
     startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-
-}
-function selectAnswer() {
-
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
 
-var questions =
-    [{ question: "Do you like JavaScript" }];
+function selectAnswer(e) {
+
+}
+
+const questions = [
+    {
+        question: "What is 2 + 2",
+        answers: [
+            { text: "4", correct: true },
+            { text: "22", correct: false }
+        ]
+    }
+]
+//var questions =
+   // [{ question: "Do you like JavaScript" }];
