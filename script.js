f99750a97f2bc748093e39f769acc16eb3324025
Mo Civ <mocivers@comcast.net>
2020-02-12 00:08:25 -0500

    added next button and activated it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
 const questionContainerElement = document.getElementById("question-container")
 const questionElement = document.getElementById("question")
 const answerButtonsElement = document.getElementById("answer-buttons")
@@ -8,7 +9,6 @@ let shuffledQuestions, currentQuestionIndex
 startButton.addEventListener("click", startGame)
 
 function startGame() {
-    console.log("Started")
     startButton.classList.add("hide")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -17,6 +17,7 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
@@ -34,18 +35,41 @@ function showQuestion(question) {
     })
 }
 
-function selectAnswer(e) {
-
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
-const questions = [
-    {
-        question: "What is 2 + 2",
-        answers: [
-            { text: "4", correct: true },
-            { text: "22", correct: false }
-        ]
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove("hide")
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+     } else {
+            element.classList.add("wrong")
+        }
     }
-]
-//var questions =
-   // [{ question: "Do you like JavaScript" }];
+    function clearStatusClass(element) {
+        element.classList.remove("correct")
+        element.classList.remove("wrong")
+    }
+
+    const questions = [
+        {
+            question: "What is the current President",
+            answers: [
+                { text: "Trump", correct: true },
+                { text: "Sanders", correct: false }
+            ]
+        }
+    ]
